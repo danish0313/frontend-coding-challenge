86fff22b29905e60185a2db0e9ad0421228137f8
danish0313 <danishoo.dk@gmail.com>
2025-09-24 22:20:42 +0200

    unit test added for stepper.tsx

--- __tests__/components/stepper/stepper.test.tsx
+++ __tests__/components/stepper/stepper.test.tsx
@@ -1,6 +1,45 @@
-// TODO: Implement your tests here
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import Stepper from "../../../components/stepper/stepper";
+
 describe("Stepper", () => {
-	it("TODO", () => {
-		expect(true).toBe(false);
-	});
+
+	// dummy data for steps
+  const steps = [
+    { title: "Step One" },
+    { title: "Step Two" },
+    { title: "Step Three" },
+  ];
+   
+  it("renders all step titles", () => {
+    render(<Stepper steps={steps} currentStep={1} />);
+    steps.forEach((step) => {
+      expect(screen.getByText(step.title)).not.toBeNull();
+    });
+  });
+
+  it("highlights the current step correctly", () => {
+    render(<Stepper steps={steps} currentStep={1} />);
+
+    // testing circles
+    const currentStepCircle = screen.getByText("2");
+    expect(currentStepCircle.classList.contains("bg-blue-600")).toBe(true);
+    expect(currentStepCircle.classList.contains("text-white")).toBe(true);
+
+    const otherStepCircle = screen.getByText("1");
+
+	expect(otherStepCircle.classList.contains("bg-white")).toBe(true);
+	expect(otherStepCircle.classList.contains("text-gray-600")).toBe(true);
+
+    const currentStepLabel = screen.getByText("Step Two");
+    expect(currentStepLabel.classList.contains("text-blue-600")).toBe(true);
+  });
+
+   // testing correct number of circles
+  it("renders the correct number of step circles", () => {
+    render(<Stepper steps={steps} currentStep={0} />);
+    const circles = screen.getAllByText(/^[1-3]$/);
+    expect(circles.length).toBe(3);
+  });
 });
+
